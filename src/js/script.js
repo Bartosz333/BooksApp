a41c13bc3f9c7c19a29295aa82b1e0ad05345480
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-04-26 10:10:25 +0200

    add initActions & edit on OOP

--- src/js/script.js
+++ src/js/script.js
@@ -1,67 +1,86 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+{
+  ('use strict');
+  const select = {
+    templateOf: {
+      booksProduct: '#template-book',
+    },
+    containerOf: {
+      books: '.books-list',
+      images: '.book__image',
+    },
+  };
+  // reference to template and list
+  const templates = {
+    templateBooks: Handlebars.compile(
+      document.querySelector(select.templateOf.booksProduct).innerHTML
+    ),
+  };
+  class Books {
+    constructor() {
+      const thisBooks = this;
 
-('use strict');
-// reference to template and list
-const select = {
-  templateOf: {
-    booksProduct: '#template-book',
-  },
-  containerOf: {
-    books: '.books-list',
-  },
-  imageWrapper: {
-    images: '.book__image',
-  },
-};
+      thisBooks.initData();
+      thisBooks.getElement();
+      thisBooks.render();
+      thisBooks.initActions();
+    }
 
-const templates = {
-  templateBooks: Handlebars.compile(
-    document.querySelector(select.templateOf.booksProduct).innerHTML
-  ),
-};
+    initData() {
+      const thisBooks = this;
 
-const booksData = dataSource.books;
+      thisBooks.booksData = dataSource.books;
+      thisBooks.favoriteBooks = []; //Add an empty array
+    }
 
-const books = document.querySelector(select.containerOf.books);
-// inside the for of loop, I go through each item from dataSource.books
-function render() {
-  for (let book of booksData) {
-    /* generating HTML code based on a template and data about a specific book. */
-    const generatedHTML = templates.templateBooks({
-      id: book.id,
-      name: book.name,
-      price: book.price,
-      rating: book.rating,
-      image: book.image,
-    });
-    /* creating a DOM element */
-    const element = utils.createDOMFromHTML(generatedHTML);
-    // creating a DOM element from HTML code
-    books.appendChild(element);
+    getElement() {
+      const thisBooks = this;
+
+      thisBooks.bookContainer = document.querySelector(
+        select.containerOf.books
+      );
+    }
+    // inside the for of loop, I go through each item from dataSource.books
+    render() {
+      const thisBooks = this;
+
+      for (let book of thisBooks.booksData) {
+        /* generating HTML code based on a template and data about a specific book. */
+        const generatedHTML = templates.templateBooks({
+          id: book.id,
+          name: book.name,
+          price: book.price,
+          rating: book.rating,
+          image: book.image,
+        });
+        /* creating a DOM element */
+        const element = utils.createDOMFromHTML(generatedHTML);
+        // creating a DOM element from HTML code
+        thisBooks.bookContainer.appendChild(element);
+      }
+    }
+
+    // Add the initActions function
+    initActions() {
+      const thisBooks = this;
+
+      /**For each item in that list, add an addEventListener that will trigger a function when detected that will run... */
+      thisBooks.bookContainer.addEventListener('dblclick', function (e) {
+        e.preventDefault(); //block default browser action (preventDefault)
+
+        const image = e.target.offsetParent;/**is just a reference to the element that participated in the event! That is, for the dblClick of the element that was clicked! */
+        const dataId = image.getAttribute('data-id');
+
+        if (!thisBooks.favoriteBooks.includes(dataId)) {
+          image.classList.add('favorite'); //removes/adds the 'favorite' class to the clicked item
+          thisBooks.favoriteBooks.push(dataId); //will add this ID to favoriteBooks[]
+        } else {
+          const indexOfBooks = thisBooks.favoriteBooks.indexOf(dataId);
+          thisBooks.favoriteBooks.splice(indexOfBooks, 1);
+          image.classList.remove('favorite');//removes/adds the 'favorite' class to the clicked item
+        }
+      });
+    }
   }
+  new Books();
 }
-
-render();
-
-const favoriteBooks = []; //Add an empty array
-
-// Add the initActions function
-function initActions() {
-  /**In it, prepare a reference to the list of all .book__image items in the .booksList */
-  const images = document.querySelectorAll(select.imageWrapper.images);
-
-  /**Then go through each item in that list. */
-  for (const image of images) {
-    /**For each item in that list, add an addEventListener that will trigger a function when detected that will run... */
-    image.addEventListener('dblclick', function (e) {
-      e.preventDefault();//block default browser action (preventDefault)
-
-      image.classList.toggle('favorite');//removes/adds the 'favorite' class to the clicked item (toggle)
-
-      const dataId = image.getAttribute('data-id');//will fetch the id of the book from its 'data-id'
-
-      favoriteBooks.push(dataId);//will add this ID to favoriteBooks[]
-    });
-  }
-}
-initActions();
