b03ee111fef18ef8c3e6647075f4eac089fadcaf
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-04-26 10:50:25 +0200

    Add booksFilter

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,7 @@
     containerOf: {
       books: '.books-list',
       images: '.book__image',
+      filters: '.filters',
     },
   };
   // reference to template and list
@@ -31,6 +32,7 @@
 
       thisBooks.booksData = dataSource.books;
       thisBooks.favoriteBooks = []; //Add an empty array
+      thisBooks.filters = [];
     }
 
     getElement() {
@@ -52,6 +54,8 @@
           price: book.price,
           rating: book.rating,
           image: book.image,
+          details: book.details.adults,
+          nonFiction: book.details.nonFiction,
         });
         /* creating a DOM element */
         const element = utils.createDOMFromHTML(generatedHTML);
@@ -64,21 +68,45 @@
     initActions() {
       const thisBooks = this;
 
-      /**For each item in that list, add an addEventListener that will trigger a function when detected that will run... */
+      /**Add a listener that waits for the clicked item (event.target), paying attention to its parent (offsetParent)*/
       thisBooks.bookContainer.addEventListener('dblclick', function (e) {
         e.preventDefault(); //block default browser action (preventDefault)
+        const image = e.target.offsetParent;
 
-        const image = e.target.offsetParent;/**is just a reference to the element that participated in the event! That is, for the dblClick of the element that was clicked! */
         const dataId = image.getAttribute('data-id');
 
         if (!thisBooks.favoriteBooks.includes(dataId)) {
-          image.classList.add('favorite'); //removes/adds the 'favorite' class to the clicked item
+          image.classList.add('favorite'); //removes/adds the 'favorite' class
           thisBooks.favoriteBooks.push(dataId); //will add this ID to favoriteBooks[]
         } else {
-          const indexOfBooks = thisBooks.favoriteBooks.indexOf(dataId);
-          thisBooks.favoriteBooks.splice(indexOfBooks, 1);
-          image.classList.remove('favorite');//removes/adds the 'favorite' class to the clicked item
+          const indexOfBooks = thisBooks.favoriteBooks.indexOf(dataId); //download index
+          thisBooks.favoriteBooks.splice(indexOfBooks, 1); //remove id
+          image.classList.remove('favorite'); //removes/adds the 'favorite' class
         }
+      });
+
+      const booksFilter = document.querySelector(select.containerOf.filters);
+
+      booksFilter.addEventListener('click', function (cb) {
+        const clickedElement = cb.target;
+
+        if (
+          clickedElement.tagName == 'INPUT' &&
+          clickedElement.type == 'checkbox' &&
+          clickedElement.name == 'filter'
+        ) {
+          console.log('clickedElement', clickedElement.valuebooksFilter);
+
+          if (clickedElement.checked) {
+            thisBooks.filters.push(clickedElement.value);
+          } else {
+            const indexOfValue = thisBooks.filters.indexOf(
+              clickedElement.value
+            );
+            thisBooks.filters.splice(indexOfValue, 1);
+          }
+        }
+        
       });
     }
   }
